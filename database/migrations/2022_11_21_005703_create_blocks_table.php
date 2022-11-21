b583e325f31a5957bada7f5b611b36d18f3ab1eb
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-20 20:00:40 -0500

    feat: added blocks schema

---
+++ database/migrations/2022_11_21_005703_create_blocks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->foreignId('user_id')->constrained('users', 'id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blocks');
+    }
+};
