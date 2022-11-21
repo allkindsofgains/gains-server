7a67483af2ffb286b2630610b47d92873f87e22f
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-20 21:06:16 -0500

    chore: nullable fields on gains meta

--- database/migrations/2022_11_21_015021_create_gains_metas_table.php
+++ database/migrations/2022_11_21_015021_create_gains_metas_table.php
@@ -14,8 +14,8 @@ return new class extends Migration {
     {
         Schema::create('gains_metas', function (Blueprint $table) {
             $table->id();
-            $table->string('object');
-            $table->string('object_id');
+            $table->string('object')->nullable();
+            $table->string('object_id')->nullable();
             $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
             $table->json('json')->nullable();
             $table->softDeletes();
