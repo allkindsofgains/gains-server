535f493d8355d4c26c464fef3025e3888a765ccf
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-20 21:07:38 -0500

    feat: added activities table

---
+++ database/migrations/2022_11_21_020634_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('object');
+            $table->string('object_id');
+            $table->string('description');
+            $table->foreignId('user_id')->constrained('users', 'id')->cascadeOnDelete();
+            $table->json('json')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+};
