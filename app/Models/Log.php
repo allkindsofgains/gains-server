d6431dc77f852dbf586806b35276d7862ea56853
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-23 01:20:17 -0500

    chore: scaffold log, session, and video relationship

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Log extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function sets(): HasMany
+    {
+        return $this->hasMany(Set::class);
+    }
+
+    public function video(): HasOne
+    {
+        return $this->hasOne(Video::class);
+    }
 }
--- app/Models/Session.php
+++ app/Models/Session.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Session extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+    public function logs(): HasMany
+    {
+        return $this->hasMany(Log::class);
+    }
 }
