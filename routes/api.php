6e65df1aaa000fa381793c51c7c561da90b534cd
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-23 01:18:00 -0500

    feat: added health check end-point

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,25 @@ use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/health', fn() => response()->json(['message' => 'ok']));
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::prefix('sessions')->group(function () {
-        Route::get('/', [SessionController::class, 'index'])->name('sessions.index');
-        Route::post('/', [SessionController::class, 'store'])->name('sessions.store');
-        Route::get('/{session}', [SessionController::class, 'show'])->name('sessions.show');
-        Route::put('/{session}', [SessionController::class, 'update'])->name('sessions.update');
-        Route::delete('/{session}', [SessionController::class, 'destroy'])->name('sessions.destroy');
+    Route::prefix('/v1')->group(function () {
+        Route::prefix('/sessions')->group(function () {
+            Route::get('/', [SessionController::class, 'index'])
+                ->name('sessions.index');
+
+            Route::post('/', [SessionController::class, 'store'])
+                ->name('sessions.store');
+
+            Route::get('/{session}', [SessionController::class, 'show'])
+                ->name('sessions.show');
+
+            Route::put('/{session}', [SessionController::class, 'update'])
+                ->name('sessions.update');
+
+            Route::delete('/{session}', [SessionController::class, 'destroy'])
+                ->name('sessions.destroy');
+        });
     });
 });
