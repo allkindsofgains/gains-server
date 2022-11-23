3db15fd1a7d1fc36a943c7da424be6e003641386
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-23 01:34:07 -0500

    feat: scaffold /api/v1/auth

---
+++ app/Http/Controllers/API/V1/AuthenticationController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function forgotPassword($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function resetPassword($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,25 +4,47 @@ use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// /api/health
 Route::get('/health', fn() => response()->json(['message' => 'ok']));
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::prefix('/v1')->group(function () {
+
+        // /sessions
         Route::prefix('/sessions')->group(function () {
             Route::get('/', [SessionController::class, 'index'])
                 ->name('sessions.index');
 
             Route::post('/', [SessionController::class, 'store'])
-                ->name('sessions.store');
+                ->name('api.v1.sessions.store');
 
             Route::get('/{session}', [SessionController::class, 'show'])
-                ->name('sessions.show');
+                ->name('api.v1.sessions.show');
 
             Route::put('/{session}', [SessionController::class, 'update'])
-                ->name('sessions.update');
+                ->name('api.v1.sessions.update');
 
             Route::delete('/{session}', [SessionController::class, 'destroy'])
-                ->name('sessions.destroy');
+                ->name('api.v1.sessions.destroy');
         });
+
+        // /auth
+        Route::prefix('/auth')->group(function () {
+            Route::get('/login', [SessionController::class, 'login'])
+                ->name('api.v1.auth.login');
+
+            Route::post('/register', [SessionController::class, 'register'])
+                ->name('api.v1.auth.register');
+
+            Route::get('/logout', [SessionController::class, 'logout'])
+                ->name('api.v1.auth.logout');
+
+            Route::put('/forgot-password', [SessionController::class, 'forgotPassword'])
+                ->name('api.v1.auth.forgot.password');
+
+            Route::delete('/reset-password', [SessionController::class, 'resetPassword'])
+                ->name('api.v1.auth.reset.password');
+        });
+
     });
 });
